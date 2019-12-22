39351a21a39a3ae2cd8061e05a70517e24b3040e
Gene Sy <gnsyftw@gmail.com>
2019-12-22 20:16:23 +0800

    Adds args and some pbe items

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,46 @@ import { parseItem } from './utils/parseItem';
 import download from './utils/download';
 import Item from './models/ItemModel';
 import saveToJson from './utils/saveToJson';
+import { argv } from 'yargs';
 
-const latestDataUrl = 'http://raw.communitydragon.org/latest/cdragon/tft/en_us.json';
-const pbeDataUrl = 'https://raw.communitydragon.org/pbe/cdragon/tft/en_us.json';
-const fakeUrl = 'https://raw.communitydragon.org/pbe/cdragon/tft/en_us.jsons'
+const versionPaths = {
+  url: '/cdragon/tft/en_us.json',
+  version: '/code-metadata.json'
+}
 
-// const getData = async() => await axios.get(latestDataUrl);
-const data = require('./en_us.json');
-const getData = async() => await Promise.resolve({ data });
+const url = 'https://raw.communitydragon.org/';
+const version = argv.dataversion || 'latest';
+
+console.log('Using version', version);
+
+const versionUrl = `${url}${version}${versionPaths.version}`
+const dataUrl = `${url}${version}${versionPaths.url}`
+
+const getData = async() => await axios.get(dataUrl);
+const getVersion = async() => await axios.get(versionUrl)
+  .then(({ data }) => {
+    const { version } = data;
+    return version.substr(0, version.indexOf('.', version.indexOf('.') + 1));
+  });
 
 (async () => {
-  const { data } = await getData();
-  const { items, sets } = data;
+  let version, data;
+  try {
+    version = await getVersion();
+    const { data: _data } = await getData();
+    data = _data;
+  } catch(e) {
+    console.log(e);
+    return;
+  }
 
+  const { items, sets } = data;
   const iconUrls = [];
-  const newItems = [];
-  // idk what these items are;
-  const testItems = []
-  items.forEach((item: Item, index: number) => {
+  const parsedItems = [];
+  const testItems = [];
+  const errorItems = [];
+
+  items.forEach((item: any, index: number) => {
     if (
       (item.id <= 0) // negative ids are not real items. idk what htey are.
       || (item.from.length === 0 && item.id >= 100)
@@ -28,33 +50,45 @@ const getData = async() => await Promise.resolve({ data });
       testItems.push(item);
       return;
     };
-    const newItem = parseItem(item, index, (newItem) => {
+    const parsedItem = parseItem(item, index, (item) => {
       const iconUrl = 'http://raw.communitydragon.org/latest/game/' + item.icon.replace('.dds', '.png').toLowerCase();
-      newItem.icon = iconUrl.substring(iconUrl.lastIndexOf('/')+1);
+      item.icon = iconUrl.substring(iconUrl.lastIndexOf('/')+1);
       iconUrls.push(iconUrl);
-      return newItem;
+      return item;
     });
-    newItems.push(newItem);
+    if (parsedItem.error) {
+      errorItems.push(parsedItem)
+      return;
+    }
+    parsedItems.push(parsedItem);
   });
 
+  if (errorItems.length) {
+    console.error(errorItems.length + ' errors');
+    errorItems.forEach(({ name, id }) => console.error({ id, name }));
+  }
 
   const basicItems = [];
   const advancedItems = [];
 
-  newItems.forEach((item) => {
+  parsedItems.forEach((item) => {
     if (item.from.length === 0) {
       basicItems.push(item);
     } else if (item.from.length === 2) {
       advancedItems.push(item);
     } else {
-      throw item;
+      errorItems.push(item)
     }
   });
 
   const itemsToJson = {
-    basicItems,
-    advancedItems
+    version,
+    data: {
+      basicItems,
+      advancedItems
+    }
   };
+
   saveToJson({ filePath: './test.json', object: itemsToJson });
 })();
 
--- src/utils/parseItem.ts
+++ src/utils/parseItem.ts
@@ -1,49 +1,6 @@
 import Item from '../models/ItemModel';
 import { keysToLowerCase, valuesToLowerCase } from './keysToLowerCase';
-
-const _effectsMap = {
-  "3abb8549": "CritChance",
-  "c4b5579c": "DodgeChance",
-  "4724fc58": "AD",
-  "ce132611": "APPercentAmp",
-  "eac3d5c4": "CriticalStrikeAmp",
-  "2275757b": "ChanceOnHitToSilence",
-  "1e0630e9": "HPThreshold",
-  "dc44b1d7": "HealthRestore",
-  "510fdb6a": "BanishDuration",
-  "9b1e8f37": "HexRange",
-  "aa03e0b1": "AttackSpeedPerStack",
-  "48ea2af8": "AttackSpeedPerStack",
-  "6688a0d5": "DamageReflect",
-  "d0088170": "ASSlowPercentage",
-  "0cc88d45": "ChanceToSpellSteal",
-  "0e4779e5": "DodgeChancePercent",
-  "a79cf66e": "CurrentHPPhysicalDamage",
-  "a8ca7859": "AttackSpeedPercent",
-  "57706a69": "BurnPercent",
-  "97e52ce8": "BurnDuration",
-  "6df27940": "BurnDuration",
-  "98ac43ce": "CritChancePerStack",
-  "cb57edb0": "CritChancePerStack",
-  "ae49cc70": "TraitMultiplier",
-  "a56e0a21": "ChanceOnHitToShrink",
-  "c425872e": "StasisDuration",
-  "03494ad0": "ManaRestore",
-  "d0fcc895": "ADPerStack",
-  "f1d43b01": "ADPerStack",
-  "276ba2c8": "MultiplierForDamage",
-  "69fff1ab": "ManaPercentRestore",
-  "93d13af6": "ExtraBounces",
-  "4516a18d": "SilenceDuration",
-  "55ce8055": "StartingStacks",
-  "ad68ce80": "StartingStacks",
-  "2426ea7e": "ChanceOnHitToDisarm",
-  "ec9a04d1": "MaxArmySizeIncrease",
-  "e93233aa": "CleanseICD",
-  "85884551": "BouncesTooltip",
-  "ad16f688": "OmniVamp",
-  "1bb18b94": "HPPerRound"
-}
+import _effectsMap from '../itemEffectsData';
 
 const effectsMap = valuesToLowerCase(_effectsMap);
 
@@ -89,7 +46,8 @@ export const parseItem = (item, index, callback = (item, index) => item) => {
   });
 
   if (item.desc.indexOf('@') !== -1 || item.desc.indexOf('undefined') !== -1) {
-    throw item.name + ' ' + item.desc;
+    // throw item.name + ' ' + item.desc;
+    item.error = true;
   }
   return callback(item, index);
 }
